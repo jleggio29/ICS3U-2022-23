6f6cfa2f5ba6497d7607459f231b28b5c6a60f68
Jacob Leggio <jleggio@bayviewglen.ca>
2022-09-12 15:11:34 -0400

    Initial commit

---
+++ UNIT 1 - INTRODUCTION/src/day2/Example1.java
@@ -0,0 +1,22 @@
+package day2;
+
+/* one of the classes must have a main method that tells java where to start running*/
+
+public class Example1 {
+
+    public static void main(String[] args){
+        System.out.println("welcome to ap computer science");
+        System.out.println("welcome to ap computer science");
+        System.out.println("welcome to ap computer science");
+        System.out.println();
+        System.out.println();
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+//println as a new line after printing
+//print leaves the cursor where it was after it prints
+System.out.print("D");
+System.out.print("E");
+System.out.print("F");
+    }
+}
---
+++ UNIT 1 - INTRODUCTION/src/day2/Example2.java
@@ -0,0 +1,20 @@
+
+package day2;
+//naming convention
+//uppercase on first letter of each word(pascal case)
+//methods/fucntions(variables)
+//constant: all upper case_ to distinguish words
+public class Example2 {
+public static void main(String[] args) {
+//we must escape the quote inside the round brackets because quotes have meaning in java
+//backslash is used to escape characters
+System.out.println("Computer Science is the \"best\" course ever");
+//there are a small subset of characters that can be escaped
+//but the AP exam will only test you on 3 of them // /* \n */
+//to display a backslash you must escape
+System.out.println("this is \\\\a test");
+System.out.println("abcd\nefghijk");
+
+}
+
+}
