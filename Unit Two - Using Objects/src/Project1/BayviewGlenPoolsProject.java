424447c1f3443b9356c54c0373697f33e7f056cc
Jacob Leggio <jleggio@bayviewglen.ca>
2022-10-16 21:31:18 -0400

    Unit two - Using Objects

--- Unit Two - Using Objects/src/Project1/BayviewGlenPoolsProject.java
+++ Unit Two - Using Objects/src/Project1/BayviewGlenPoolsProject.java
@@ -37,25 +37,23 @@ public class BayviewGlenPoolsProject {
         double transitionLength = (double) Math.sqrt(Math.pow(transitionSlope,2) - Math.pow(transitionHeight,2));
         double deepLength = (double) length - shallowLength - transitionLength ;
 
-        // Amount Of Water to Keep Pool 90% Full: (Volume Of Pool, Converted From Meters Cubed To Gallons, Multiplied By 0.9 In Order To Keep Pool 90% Full):
+        // Amount Of Water (In Gallons) Needed To Keep Pool 90% Full: (Volume Of Pool, Converted From Meters Cubed To Gallons, Multiplied By 0.9):
 
-        double amountOfWater = shallowHeight * length * width + transitionHeight * width * deepLength + 
-        width * transitionHeight * transitionLength / 2;
+        double amountOfWater = shallowHeight * length * width + transitionHeight * width * deepLength + width * transitionHeight * transitionLength / 2;
 
         double amountOfWaterInGallons = amountOfWater * 264.17205;
 
         double amountOfWaterInGallons90PercentFull = amountOfWaterInGallons * 0.9;
 
-        // Amount Of Liner Needed For Pool: (Surface Area In Meters Squared Excluding Area Of Top Of Pool)
+        // Amount Of Liner Needed For Pool: (Surface Area Of Pool In Meters Squared Excluding Area Of Top Of Pool)
 
-        double amountOfLiner = width * deepHeight + width * shallowHeight + 
-        2 * (length * shallowHeight + transitionLength * transitionHeight / 2 + deepLength * transitionHeight); 
+        double amountOfLiner = width * deepHeight + width * shallowHeight + 2 * (length * shallowHeight + transitionLength * transitionHeight / 2 + deepLength * transitionHeight); 
 
-        // Cost Of Liner Needed For Pool: (Surface Area Of Pool In Meters Squared * Price Of Liner In Dollars Per Meter Squared)
+        // Cost Of Liner Needed For Pool: (Surface Area Of Pool In Meters Squared * Price Of Liner (In Dollars) Per Meter Squared)
       
         double costLiner = amountOfLiner * linerCost; 
 
-        // Results: (Rounded To Nearest 100th of a gallon, 100th of a meter squared, and 100th of a dollar):
+        // Printed Results: (Rounded To Nearest 100th of a gallon, 100th of a meter squared, and 100th of a dollar):
 
         System.out.println("The amount of water needed to keep the pool 90% full is " + Math.round(amountOfWaterInGallons90PercentFull * 100) / 100.0 + " gallons.");
         System.out.println("The amount of liner needed to create the pool is " + Math.round(amountOfLiner * 100) / 100.0 + " meters squared.");
