dae5149c620c43ca88af2955621e4039eb909413
Jacob Leggio <jleggio@bayviewglen.ca>
2022-10-17 19:02:21 -0400

    Unit Two - Using Objects

--- Unit Two - Using Objects/src/Project1/BayviewGlenPoolsProject.java
+++ Unit Two - Using Objects/src/Project1/BayviewGlenPoolsProject.java
@@ -41,13 +41,13 @@ public class BayviewGlenPoolsProject {
 
         double amountOfWater = shallowHeight * length * width + transitionHeight * width * deepLength + width * transitionHeight * transitionLength / 2;
 
-        double amountOfWaterInGallons = amountOfWater * 264.17205;
+        double amountOfWater90PercentFull = amountOfWater * 0.9;
 
-        double amountOfWaterInGallons90PercentFull = amountOfWaterInGallons * 0.9;
+        double amountOfWaterInGallons = amountOfWater90PercentFull * 264.17205;
 
         // Amount Of Liner Needed For Pool: (Surface Area Of Pool In Meters Squared Excluding Area Of Top Of Pool)
 
-        double amountOfLiner = width * deepHeight + width * shallowHeight + 2 * (length * shallowHeight + transitionLength * transitionHeight / 2 + deepLength * transitionHeight); 
+        double amountOfLiner = width * deepHeight + width * shallowHeight + width * transitionSlope + width * shallowLength + width * deepLength + 2 * (length * shallowHeight + transitionLength * transitionHeight / 2 + deepLength * transitionHeight); 
 
         // Cost Of Liner Needed For Pool: (Surface Area Of Pool In Meters Squared * Price Of Liner (In Dollars) Per Meter Squared)
       
@@ -55,7 +55,7 @@ public class BayviewGlenPoolsProject {
 
         // Printed Results: (Rounded To Nearest 100th of a gallon, 100th of a meter squared, and 100th of a dollar):
 
-        System.out.println("The amount of water needed to keep the pool 90% full is " + Math.round(amountOfWaterInGallons90PercentFull * 100) / 100.0 + " gallons.");
+        System.out.println("The amount of water needed to keep the pool 90% full is " + Math.round(amountOfWaterInGallons * 100) / 100.0 + " gallons.");
         System.out.println("The amount of liner needed to create the pool is " + Math.round(amountOfLiner * 100) / 100.0 + " meters squared.");
         System.out.println("The total cost of the liner needed to create the pool is $" + Math.round(costLiner * 100) / 100.0 + ".");
 
